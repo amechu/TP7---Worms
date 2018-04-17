d77e976f515e9da81b3df8322c4918b5c08e30c8
Alan <31071359+amechu@users.noreply.github.com>
2018-04-17 19:10:48 -0300

    Hola

---
+++ Worms/Worms/Main.cpp
@@ -0,0 +1,13 @@
+/*
+
+Se utiliza la misma FSM del otro TP pero ahora hay que implementarla con un contenido.
+
+Model View Controller (MVC) //Mirar
+
+Como empezariamos este TP?
+
+Event Generator, Clase evento? (ya la tengo hecha)
+
+
+
+*/
